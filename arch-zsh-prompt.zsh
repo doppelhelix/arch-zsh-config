c800ff0b21286a5b08884ea220139fd2a84dbbed
Christian Schendel <doppelhelix@gmail.com>
2024-07-19 22:28:30 +0200

    complete rewrite of arch-zsh-prompt.zsh 	modified:   arch-zsh-prompt.zsh

--- arch-zsh-prompt.zsh
+++ arch-zsh-prompt.zsh
@@ -1,31 +1,30 @@
 () {
-  emulate -L zsh
-
-  source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
-  source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-  # Determine terminal capabilities.
-  {
-    if ! zmodload zsh/langinfo zsh/terminfo ||
-       [[ $langinfo[CODESET] != (utf|UTF)(-|)8 || $TERM == (dumb|linux) ]] ||
-       (( terminfo[colors] < 256 )); then
-      # Don't use the powerline config. It won't work on this terminal.
-      local USE_POWERLINE=false
-      # Define alias `x` if our parent process is `login`.
-      local parent
-      if { parent=$(</proc/$PPID/comm) } && [[ ${parent:t} == login ]]; then
-        alias x='startx ~/.xinitrc'
-      fi
-    fi
-  } 2>/dev/null
-
-  if [[ $USE_POWERLINE == false ]]; then
-    # Use 8 colors and ASCII.
-    source /usr/share/zsh/p10k-portable.zsh
-    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,bold'
-  else
-    # Use 256 colors and UNICODE.
-    source /usr/share/zsh/p10k.zsh
-    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
-  fi
+    emulate -L zsh
+    # Determine terminal capabilities.
+    {
+        if
+            ! zmodload zsh/langinfo zsh/terminfo ||
+            [[ $langinfo[CODESET] != (utf|UTF)(-|)8 ||
+            $TERM == (dumb|linux) ]] ||
+            (( terminfo[colors] < 256 ));
+        then
+            clear
+            source $HOME/.config/zsh/p10k-portable.zsh
+            base16_mocha_neu
+            ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,bold'
+            fastfetch --load-config $HOME/.config/fastfetch/config-tty.jsonc
+            # Don't use the powerline config. It won't work on this terminal.
+            local USE_POWERLINE=false
+        else
+            # Use 256 colors and UNICODE.
+            clear
+            source $HOME/.config/zsh/p10k.zsh
+            base16_mocha_neu
+            ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
+            fastfetch
+        fi
+    } 2>/dev/null
 }
+
+# vim:filetype=zsh foldmethod=marker autoindent expandtab tabstop=4 softtabstop=4 shiftwidth=4
+# shellcheck shell=zsh
